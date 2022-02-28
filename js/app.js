98563b02fae2650e5648c3c943c75eb0d09bdb41
dev_nayan <md.nayan8494@gmail.com>
2022-03-01 01:50:45 +0600

    Show api data on ui successfully

--- js/app.js
+++ js/app.js
@@ -8,9 +8,26 @@ const loadData = () => {
     const apiLink = `https://openapi.programming-hero.com/api/phones?search=${searchData}`;
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchData}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => showData(data))
 }
 
+const showData = (phones) => {
+    console.log(phones);
+    phones.data.forEach(phone => {
+        const results = document.getElementById('results') 
+        const phoneContainer = document.createElement('div');
+        phoneContainer.classList.add = 'col';
+        phoneContainer.innerHTML = `
+        <div class="card h-100">
+            <img src=${phone.image} class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">Brand : ${phone.brand}</p>
+            </div>
+        </div>`;
+        results.appendChild(phoneContainer);
+    })
+}
 
 
 // Event Handlers
