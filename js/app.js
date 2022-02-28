4b369647e38f9e16e6d04d032f44114a617f534e
dev_nayan <md.nayan8494@gmail.com>
2022-03-01 01:45:50 +0600

    Call the api successfully

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Hello World!')
+// Define the variables
+const searchBtn = document.getElementById('search-btn');
+const searchField = document.getElementById('search-field');
+
+// Functions
+const loadData = () => {
+    const searchData = searchField.value;
+    const apiLink = `https://openapi.programming-hero.com/api/phones?search=${searchData}`;
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchData}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+
+
+// Event Handlers
+searchBtn.addEventListener('click', loadData);
+
