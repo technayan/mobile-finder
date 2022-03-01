dce2aaa4ffbcb7bf6647ebbfff20d2045d6b0cfc
dev_nayan <md.nayan8494@gmail.com>
2022-03-01 16:15:43 +0600

    Load more button added and its functionality done

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const searchBtn = document.getElementById('search-btn');
 const searchField = document.getElementById('search-field');
 const results = document.getElementById('results');
+const loadMoreBtn = document.getElementById('load-more-btn');
 
 // Functions
 const loadData = () => {
@@ -33,15 +34,70 @@ const showData = (phones) => {
                     <button class="btn btn-primary" id="phone-btn">Explore</button>
                 </div>
             </div>`;
-            results.appendChild(phoneContainer);
+            results.appendChild(phoneContainer); 
             
         })
-        
+
+
+        loadMoreBtn.style.display = 'block'; 
+
+
+        loadMoreBtn.addEventListener('click', loadMoreData);
+
+        let afterTwenty = resultsQnt.slice(20,resultsQnt.length );
+
+        function loadMoreData() {
+            
+            
+            afterTwenty.forEach(phone => {
+                const phoneContainer = document.createElement('div');
+                phoneContainer.classList.add = 'col';
+                phoneContainer.innerHTML = `
+                <div class="card h-100">
+                    <img src=${phone.image} class="card-img-top phone-img my-3 mx-auto w-50" alt="${phone.phone_name}">
+                    <div class="card-body phone-desc d-flex justify-content-between align-items-center">
+                        <div>
+                            <h5 class="card-title">${phone.phone_name}</h5>
+                            <p class="card-text">Brand : ${phone.brand}</p>
+                        </div>
+                        <button class="btn btn-primary" id="phone-btn">Explore</button>
+                    </div>
+                </div>`;
+                results.appendChild(phoneContainer); 
+                loadMoreBtn.style.display = 'none';
+            })
+            afterTwenty = [];
+        }
+
+
+
+    } else {
+        resultsQnt.forEach(phone => {
+            const phoneContainer = document.createElement('div');
+            phoneContainer.classList.add = 'col';
+            phoneContainer.innerHTML = `
+            <div class="card h-100">
+                <img src=${phone.image} class="card-img-top phone-img my-3 mx-auto w-50" alt="${phone.phone_name}">
+                <div class="card-body phone-desc d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5 class="card-title">${phone.phone_name}</h5>
+                        <p class="card-text">Brand : ${phone.brand}</p>
+                    </div>
+                    <button class="btn btn-primary" id="phone-btn">Explore</button>
+                </div>
+            </div>`;
+            results.appendChild(phoneContainer); 
+        })
     }
+
     
 }
 
 
+
+
 // Event Handlers
 searchBtn.addEventListener('click', loadData);
+
+
 
