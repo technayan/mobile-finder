df95c04f66010827a653a946ec2d2f30bdfb1775
dev_nayan <md.nayan8494@gmail.com>
2022-03-01 15:26:45 +0600

    Load 20 data and show on UI successfully

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 // Define the variables
 const searchBtn = document.getElementById('search-btn');
 const searchField = document.getElementById('search-field');
+const results = document.getElementById('results');
 
 // Functions
 const loadData = () => {
+    results.innerText = "";
     const searchData = searchField.value;
     const apiLink = `https://openapi.programming-hero.com/api/phones?search=${searchData}`;
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchData}`)
@@ -12,24 +14,31 @@ const loadData = () => {
 }
 
 const showData = (phones) => {
-    console.log(phones);
-    phones.data.forEach(phone => {
-        const results = document.getElementById('results') 
-        const phoneContainer = document.createElement('div');
-        phoneContainer.classList.add = 'col';
-        phoneContainer.innerHTML = `
-        <div class="card h-100">
-            <img src=${phone.image} class="card-img-top phone-img my-3 mx-auto w-50" alt="">
-            <div class="card-body phone-desc d-flex justify-content-between align-items-center">
-                <div>
-                    <h5 class="card-title">${phone.phone_name}</h5>
-                    <p class="card-text">Brand : ${phone.brand}</p>
+    const resultsQnt = phones.data;
+    console.log(resultsQnt);
+    if (resultsQnt.length > 20) {
+        const firstTwenty = resultsQnt.slice(0,20);
+        console.log(firstTwenty);
+        firstTwenty.forEach(phone => {
+            const phoneContainer = document.createElement('div');
+            phoneContainer.classList.add = 'col';
+            phoneContainer.innerHTML = `
+            <div class="card h-100">
+                <img src=${phone.image} class="card-img-top phone-img my-3 mx-auto w-50" alt="${phone.phone_name}">
+                <div class="card-body phone-desc d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5 class="card-title">${phone.phone_name}</h5>
+                        <p class="card-text">Brand : ${phone.brand}</p>
+                    </div>
+                    <button class="btn btn-primary" id="phone-btn">Explore</button>
                 </div>
-                <button class="btn btn-primary" id="phone-btn">Explore</button>
-            </div>
-        </div>`;
-        results.appendChild(phoneContainer);
-    })
+            </div>`;
+            results.appendChild(phoneContainer);
+            
+        })
+        
+    }
+    
 }
 
 
